50f0e0701d0c9c9f9b818fbdee2ebb3f46dcb873
Labruhtooryboi <67720330+Labruhtooryboi@users.noreply.github.com>
2021-04-13 08:43:53 -0900

    Update defsetup.sh

--- defsetup.sh
+++ defsetup.sh
@@ -23,6 +23,7 @@ read -p "What is the /path/to/chatterbox folder? (EX: /var/www/chatterbox)> " ma
 read -p "What it the /path/to/ssl/cert? (EX: /etc/letsencrypt/domain/fullhain.pem)> " certloc
 read -p "What it the /path/to/ssl/cert? (EX: /etc/letsencrypt/domain/privkey.pem)> " keyloc
 #main.go public folder edits
+echo "Editing public folder paths"
 sed -i "s+$mainloc/chat-blue/public+../public+gi" chat-blue/src/main.go
 sed -i "s+$mainloc/chat-red/public+../public+gi" chat-red/src/main.go
 sed -i "s+$mainloc/chat-green/public+../public+gi" chat-green/src/main.go
@@ -32,19 +33,13 @@ sed -i "s+$mainloc/chat-red-ssl/public+../public+gi" chat-red-ssl/src/main.go
 sed -i "s+$mainloc/chat-green-ssl/public+../public+gi" chat-green-ssl/src/main.go
 sed -i "s+$mainloc/chat-orange-ssl/public+../public+gi" chat-orange-ssl/src/main.go
 #main.go ssl cert path edits
-sed -i "s+$certloc+/full/path/to/cert+gi" chat-blue/src/main.go
-sed -i "s+$certloc+/full/path/to/cert+gi" chat-red/src/main.go
-sed -i "s+$certloc+/full/path/to/cert+gi" chat-green/src/main.go
-sed -i "s+$certloc+/full/path/to/cert+gi" chat-orange/src/main.go
+echo "Editing ssl cert folder paths"
 sed -i "s+$certloc+/full/path/to/cert+gi" chat-blue-ssl/src/main.go
 sed -i "s+$certloc+/full/path/to/cert+gi" chat-red-ssl/src/main.go
 sed -i "s+$certloc+/full/path/to/cert+gi" chat-green-ssl/src/main.go
 sed -i "s+$certloc+/full/path/to/cert+gi" chat-orange-ssl/src/main.go
 #main.go ssl key path edits
-sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-blue/src/main.go
-sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-red/src/main.go
-sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-green/src/main.go
-sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-orange/src/main.go
+echo "Editing ssl key folder paths"
 sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-blue-ssl/src/main.go
 sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-red-ssl/src/main.go
 sed -i "s+$keyloc+/full/path/to/certkey+gi" chat-green-ssl/src/main.go
